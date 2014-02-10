4cdac8304f6026be6edc8bd5135f47697c6f229e
Jordan Aupetit (Ordinateur Fixe) <aupetit.jordan@gmail.com>
2014-02-11 00:10:08 +0100

    Test sur la page d'accueil

--- application/views/pages/accueil.php
+++ application/views/pages/accueil.php
@@ -1,4 +1,4 @@
-<div style="width:500px; height:500px; position:absolute; margin:auto; left:0; right:0;"> 
+<div style="width:500px; height:600px; position:absolute; margin:auto; left:0; right:0;"> 
     <div class="row">
         <div style="background:; width:100%; text-align:center; font-size:50px; position: absolute; top:100px;">Jordan Aupetit</div>
         <div style="background:; width:100%; text-align:center; font-size:20px; position: absolute; top:170px;">Développeur - Étudiant en Informatique</div>
@@ -15,12 +15,16 @@
             </a>
         </div>
 
-        <div class="hidden-xs hidden-sm" style="text-align:center; width:900px; position: absolute; bottom: 50px; left: 50%; margin-left: -450px;">
-            <button class="btn btn-accueil btn-default btn-page-jordan">Mes compétences</button>
+        <div class="hidden-xs hidden-sm" style="text-align:center; width:810px; position: absolute; bottom: 0; left: 50%; margin-left: -405px;">
+            <!-- <button class="btn btn-accueil btn-default btn-page-jordan">Mes compétences</button>
             <button class="btn btn-accueil btn-default btn-page-projets">Mes projets</button>
             <a href="<?php echo base_url()."assets/pdf/CV_Jordan_Aupetit_Version_2014.pdf";?>">
                 <button class="btn btn-accueil btn-default">Mon CV</button> 
-            </a>
+            </a> -->
+
+            <div style="width:250px; height:150px; background:rgb(241, 241, 241); border:1px solid rgba(0, 0, 0, 0.17); float:left; margin:10px;"></div>
+            <div style="width:250px; height:150px; background:rgb(241, 241, 241); border:1px solid rgba(0, 0, 0, 0.17); float:left; margin:10px;"></div>
+            <div style="width:250px; height:150px; background:rgb(241, 241, 241); border:1px solid rgba(0, 0, 0, 0.17); float:left; margin:10px;"></div>
         </div>
     </div>
 </div>
